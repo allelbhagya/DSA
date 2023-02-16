def173e9382bc8a3ef96cc54f155261b752d57e8
Bhagyalaxmi <80905783+allelbhagya@users.noreply.github.com>
2023-02-16 17:12:21 +0530

    Add files via upload

---
+++ linear data structure/stack.py
@@ -0,0 +1,34 @@
+def stack():
+    #initializing the stack
+    stack = []
+    return stack
+
+def check_empty(stack):
+    #checking if the stack is empty
+    if len(stack)==0:
+        return True
+    else:
+        return False
+def push(stack,item):
+    stack.append(item)
+    # adding the item to the end of the stack
+    print("item pushed is: "+item)
+
+def pop(stack):
+    if (check_empty(stack)):
+        return "the stack is empty"
+    #else remove the last element
+    return stack.pop() # and return the element
+def print_stack(stack):
+    print(*stack)
+
+
+s = stack()
+push(s, str(1))
+push(s, str(2))
+push(s, str(3))
+push(s,str(4))
+pop(s)
+print_stack(s)
+push(s, str(5))
+print_stack(s)
